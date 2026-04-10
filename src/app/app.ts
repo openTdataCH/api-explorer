50d136baec4feedaa30b0c8a228d4da8845807b6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-04-10 13:42:15 +0200

    Simplified App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [RouterOutlet],
 })
 export class App {
-  protected readonly title = signal('api-explorer');
+  public constructor() {
+
+  }
 }
