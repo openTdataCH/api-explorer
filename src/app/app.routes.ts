cc31edb3b7ff130a8c65799a99a0e246b87b85b6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-04-10 13:47:30 +0200

    Updates routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlMatcher, UrlSegment } from '@angular/router';
 
-export const routes: Routes = [];
+import { Detail } from './detail/detail';
+import { Index } from './index';
+import { AppHelpers } from './helpers/app.helpers';
+
+export const apiIdMatcher: UrlMatcher = (segments: UrlSegment[]) => {
+  if (segments.length !== 1) {
+    return null;
+  }
+
+  const apiId = segments[0].path;
+  const apiConfig = AppHelpers.matchApiConfig(apiId);
+  if (!apiConfig) {
+    return null;
+  }
+
+  return {
+    consumed: segments,
+    posParams: {
+      id: segments[0],
+    },
+  };
+};
+
+export const routes: Routes = [
+  {
+    matcher: apiIdMatcher,
+    component: Detail,
+  },
+  {
+    path: '**',
+    component: Index,
+  },
+];
+
