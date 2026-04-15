e46439dc45876c9a57f1e9f5b33cbdc88909daec
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-04-15 12:21:19 +0200

    Use relative paths

--- src/app/config/api-config.ts
+++ src/app/config/api-config.ts
@@ -8,26 +8,26 @@ export const API_Data: API_Config[] = [
   {
     id: 'formation',
     title: 'Formation Service',
-    yamlPath: '/openapi/formation/openapi.template.yaml',
+    yamlPath: './openapi/formation/openapi.template.yaml',
   },
   {
     id: 'ojp1.0',
     title: 'OJP 1.0',
-    yamlPath: '/openapi/ojp1.0/openapi.template.yaml',
+    yamlPath: './openapi/ojp1.0/openapi.template.yaml',
   },
   {
     id: 'ojp2.0',
     title: 'OJP 2.0',
-    yamlPath: '/openapi/ojp2.0/openapi.template.yaml',
+    yamlPath: './openapi/ojp2.0/openapi.template.yaml',
   },
   {
     id: 'ojpfare',
     title: 'OJP Fare',
-    yamlPath: '/openapi/ojpfare/openapi.template.yaml',
+    yamlPath: './openapi/ojpfare/openapi.template.yaml',
   },
   {
     id: 'traffic_lights',
     title: 'Traffic lights',
-    yamlPath: '/openapi/traffic_lights/openapi.template.yaml',
+    yamlPath: './openapi/traffic_lights/openapi.template.yaml',
   },
 ];
